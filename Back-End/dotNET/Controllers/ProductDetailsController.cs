038b2a3f1b629ab7afea8bbbf9531b0e767fe643
elmalkimohammed <mohammedelmalki241@gmail.com>
2025-12-26 23:41:28 +0100

    Finished Implementing The Logic For Our Controller To Handle Requests Regarding A Product's Specific Informations

--- Back-End/dotNET/Controllers/ProductDetailsController.cs
+++ Back-End/dotNET/Controllers/ProductDetailsController.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using dotNET.Models;
+using dotNET.Repository;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace dotNET.Controllers
@@ -7,5 +9,41 @@ namespace dotNET.Controllers
     [ApiController]
     public class ProductDetailsController : ControllerBase
     {
+        // Dependency injection of the product repository
+        private readonly productRepository _repository;
+
+        public ProductDetailsController(productRepository repo)
+        {
+            _repository = repo;
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetProductById(string requestedId)
+        {
+            // Fetch the product details from the repository
+            var searchedProd = await _repository.GetProductById(requestedId) ;
+
+            // Failing to find the product returns null
+            if (searchedProd == null )
+            {
+                return NotFound();
+            }
+
+            // Create an anonymous object to shape the response
+            var response = new
+            {
+                searchedProd.Id,
+                searchedProd.Name,
+                searchedProd.Description,
+                searchedProd.Price,
+                searchedProd.Stock,
+                searchedProd.Attributes,
+                searchedProd.Images
+            };
+
+            // Return the shaped response
+            return Ok(response);
+
+        }
     }
 }
