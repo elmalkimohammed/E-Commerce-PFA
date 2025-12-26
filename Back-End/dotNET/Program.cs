0614154eb3b32ea1916a34e9589b4b262388535e
elmalkimohammed <mohammedelmalki241@gmail.com>
2025-12-26 21:23:08 +0100

    Deleted The Old .NET Folder's Structure And Created A New One That Follows Our WorkFlow

--- Back-End/dotNET/Program.cs
+++ Back-End/dotNET/Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+builder.Services.AddOpenApi();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.MapOpenApi();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
